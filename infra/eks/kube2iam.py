94887dbfbe4f01e7486304db18c2293c34b993a2
Calum Barnett <46442591+calumabarnett@users.noreply.github.com>
2021-09-21 16:19:18 +0100

    Bump and enforce dev versions (#3)
    * Bump and enforce dev versions
    
    * Update README.md
    
    * Update requirements
    
    * Update regex for roles that can be assumed
    
    * Update prepare kube config

--- infra/eks/kube2iam.py
+++ infra/eks/kube2iam.py
@@ -1,7 +1,7 @@
 import pulumi_kubernetes as k8s
 from pulumi.resource import ResourceOptions
 
-from ..base import region
+from ..base import eks_config, region
 from ..iam.roles import instanceRole
 from .cluster import cluster
 
@@ -9,6 +9,8 @@ chart = k8s.helm.v3.Chart(
     release_name="kube2iam",
     config=k8s.helm.v3.ChartOpts(
         chart="kube2iam",
+        fetch_opts=k8s.helm.v3.FetchOpts(repo="https://jtblin.github.io/kube2iam"),
+        namespace="kube-system",
         values={
             "aws": {"region": region},
             "rbac": {"create": True},
@@ -22,7 +24,7 @@ chart = k8s.helm.v3.Chart(
             },
             "host": {"iptables": True, "interface": "eni+"},
         },
-        fetch_opts=k8s.helm.v3.FetchOpts(repo="https://jtblin.github.io/kube2iam"),
+        version=eks_config["kube2iam"]["chart_version"],
     ),
     opts=ResourceOptions(provider=cluster.provider, parent=cluster),
 )
