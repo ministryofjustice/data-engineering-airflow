56158da5a06a24c3ff95742b7ca526eb553b215e
calumabarnett <calum.barnett@digital.justice.gov.uk>
2021-10-01 21:06:52 +0100

    Remove commented section

--- infra/eks/kube2iam.py
+++ infra/eks/kube2iam.py
@@ -33,30 +33,3 @@ release = k8s.helm.v3.Release(
         parent=cluster,
     ),
 )
-
-# chart = k8s.helm.v3.Chart(
-#     release_name="kube2iam",
-#     config=k8s.helm.v3.ChartOpts(
-#         chart="kube2iam",
-#         fetch_opts=k8s.helm.v3.FetchOpts(repo="https://jtblin.github.io/kube2iam"),
-#         namespace="kube-system",
-#         values={
-#             "aws": {"region": region},
-#             "rbac": {"create": True},
-#             "extraArgs": {
-#                 "base-role-arn": instanceRole.arn.apply(
-#                     lambda arn: arn.split("/")[0] + "/"
-#                 ),
-#                 "default-role": instanceRole.name,
-#                 "namespace-restrictions": True,
-#                 "namespace-restriction-format": "regexp",
-#             },
-#             "host": {"iptables": True, "interface": "eni+"},
-#         },
-#         version=eks_config["kube2iam"]["chart_version"],
-#     ),
-#     opts=ResourceOptions(
-#         provider=cluster.provider,
-#         parent=cluster,
-#     ),
-# )
