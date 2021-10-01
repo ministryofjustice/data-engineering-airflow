6053a068a061976844af63b29fcf8470580afdfb
calumabarnett <calum.barnett@digital.justice.gov.uk>
2021-10-01 20:27:18 +0100

    Change charts to releases

--- infra/eks/cluster_autoscaler.py
+++ infra/eks/cluster_autoscaler.py
@@ -3,24 +3,23 @@ from pulumi import ResourceOptions
 
 from ..base import eks_config, region
 from .cluster import cluster
-from .namespaces.github_actions import clusterRoleBinding, namespace, serviceAccount
 
-clusterAutoscaler = k8s.helm.v3.Chart(
-    release_name="cluster-autoscaler",
-    config=k8s.helm.v3.ChartOpts(
-        chart="cluster-autoscaler",
-        fetch_opts=k8s.helm.v3.FetchOpts(
-            repo="https://kubernetes.github.io/autoscaler"
-        ),
-        namespace="kube-system",
-        values={
-            "autoDiscovery": {"clusterName": cluster.name},
-            "awsRegion": region,
-        },
-        version=eks_config["cluster_autoscaler"]["chart_version"],
+release = k8s.helm.v3.Release(
+    resource_name="cluster-autoscaler",
+    chart="cluster-autoscaler",
+    repository_opts=k8s.helm.v3.RepositoryOptsArgs(
+        repo="https://kubernetes.github.io/autoscaler"
     ),
+    name="cluster-autoscaler",
+    namespace="default",
+    skip_await=True,
+    values={
+        "autoDiscovery": {"clusterName": cluster.name},
+        "awsRegion": region,
+        "fullnameOverride": "cluster-autoscaler",
+    },
+    version=eks_config["cluster_autoscaler"]["chart_version"],
     opts=ResourceOptions(
-        depends_on=[clusterRoleBinding, namespace, serviceAccount],
         provider=cluster.provider,
         parent=cluster,
     ),
--- infra/eks/kube2iam.py
+++ infra/eks/kube2iam.py
@@ -4,32 +4,59 @@ from pulumi.resource import ResourceOptions
 from ..base import eks_config, region
 from ..iam.roles import instanceRole
 from .cluster import cluster
-from .namespaces.github_actions import clusterRoleBinding, namespace, serviceAccount
 
-chart = k8s.helm.v3.Chart(
-    release_name="kube2iam",
-    config=k8s.helm.v3.ChartOpts(
-        chart="kube2iam",
-        fetch_opts=k8s.helm.v3.FetchOpts(repo="https://jtblin.github.io/kube2iam"),
-        namespace="kube-system",
-        values={
-            "aws": {"region": region},
-            "rbac": {"create": True},
-            "extraArgs": {
-                "base-role-arn": instanceRole.arn.apply(
-                    lambda arn: arn.split("/")[0] + "/"
-                ),
-                "default-role": instanceRole.name,
-                "namespace-restrictions": True,
-                "namespace-restriction-format": "regexp",
-            },
-            "host": {"iptables": True, "interface": "eni+"},
-        },
-        version=eks_config["kube2iam"]["chart_version"],
+release = k8s.helm.v3.Release(
+    resource_name="kube2iam",
+    chart="kube2iam",
+    repository_opts=k8s.helm.v3.RepositoryOptsArgs(
+        repo="https://jtblin.github.io/kube2iam"
     ),
+    name="kube2iam",
+    namespace="default",
+    skip_await=True,
+    values={
+        "aws": {"region": region},
+        "rbac": {"create": True},
+        "extraArgs": {
+            "base-role-arn": instanceRole.arn.apply(
+                lambda arn: arn.split("/")[0] + "/"
+            ),
+            "default-role": instanceRole.name,
+            "namespace-restrictions": True,
+            "namespace-restriction-format": "regexp",
+        },
+        "host": {"iptables": True, "interface": "eni+"},
+    },
+    version=eks_config["kube2iam"]["chart_version"],
     opts=ResourceOptions(
-        depends_on=[clusterRoleBinding, namespace, serviceAccount],
         provider=cluster.provider,
         parent=cluster,
     ),
 )
+
+# chart = k8s.helm.v3.Chart(
+#     release_name="kube2iam",
+#     config=k8s.helm.v3.ChartOpts(
+#         chart="kube2iam",
+#         fetch_opts=k8s.helm.v3.FetchOpts(repo="https://jtblin.github.io/kube2iam"),
+#         namespace="kube-system",
+#         values={
+#             "aws": {"region": region},
+#             "rbac": {"create": True},
+#             "extraArgs": {
+#                 "base-role-arn": instanceRole.arn.apply(
+#                     lambda arn: arn.split("/")[0] + "/"
+#                 ),
+#                 "default-role": instanceRole.name,
+#                 "namespace-restrictions": True,
+#                 "namespace-restriction-format": "regexp",
+#             },
+#             "host": {"iptables": True, "interface": "eni+"},
+#         },
+#         version=eks_config["kube2iam"]["chart_version"],
+#     ),
+#     opts=ResourceOptions(
+#         provider=cluster.provider,
+#         parent=cluster,
+#     ),
+# )
