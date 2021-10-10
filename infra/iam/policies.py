5939ff6cbd0b01d904638503ff2c8844ccc3fec2
Calum Barnett <46442591+calumabarnett@users.noreply.github.com>
2021-10-10 11:32:11 +0100

    Add UI access policy (#6)
    * Enable automated deployment of K8s resources
    
    * Add UI access policy
    
    * Fix imports

---
+++ infra/iam/policies.py
@@ -0,0 +1,20 @@
+from pulumi_aws.iam import GetPolicyDocumentStatementArgs, Policy, get_policy_document
+
+from ..base import account_id, base_name, environment_name, region, tagger
+
+Policy(
+    resource_name=base_name,
+    name=f"{base_name}-ui-access",
+    policy=get_policy_document(
+        statements=[
+            GetPolicyDocumentStatementArgs(
+                actions=["airflow:CreateWebLoginToken"],
+                resources=[
+                    f"arn:aws:airflow:{region}:{account_id}:role/"
+                    f"{environment_name}/User"
+                ],
+            )
+        ]
+    ).json,
+    tags=tagger.create_tags(f"{base_name}-ui-access"),
+)
