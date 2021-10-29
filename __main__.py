f5b29cfac27aac6de6b70a9f29e25dd65ad03dcd
calumabarnett <calum.barnett@digital.justice.gov.uk>
2021-10-29 16:44:59 +0100

    Add gatekeeper

--- __main__.py
+++ __main__.py
@@ -1,18 +1,20 @@
 from infra import base, mwaa, s3, vpc
-from infra.eks import cluster, kube2iam, cluster_autoscaler
+from infra.eks import cluster, cluster_autoscaler, kube2iam
+from infra.eks.gatekeeper import gatekeeper
 from infra.eks.namespaces import airflow
-from infra.iam import role_policies, roles, policies
+from infra.iam import policies, role_policies, roles
 
 __all__ = [
     "airflow",
     "base",
-    "roles",
-    "role_policies",
-    "policies",
     "cluster",
     "cluster_autoscaler",
+    "gatekeeper",
     "kube2iam",
     "mwaa",
+    "policies",
+    "roles",
+    "role_policies",
     "s3",
     "vpc",
 ]
