0a85399a82491fdfbc8f65796e047ddfb949b00b
calumabarnett <calum.barnett@digital.justice.gov.uk>
2021-10-29 16:44:40 +0100

    Update the cluster autoscaler

--- infra/iam/roles.py
+++ infra/iam/roles.py
@@ -55,24 +55,6 @@ instanceRole = Role(
                 statements=[
                     GetPolicyDocumentStatementArgs(
                         actions=["sts:AssumeRole"],
-                        resources=[f"arn:aws:iam::{account_id}:role/airflow*"],
-                    )
-                ]
-            ).json,
-        ),
-        RoleInlinePolicyArgs(
-            name="cluster-autoscaler",
-            policy=get_policy_document(
-                statements=[
-                    GetPolicyDocumentStatementArgs(
-                        actions=[
-                            "autoscaling:DescribeAutoScalingGroups",
-                            "autoscaling:DescribeAutoScalingInstances",
-                            "autoscaling:DescribeLaunchConfigurations",
-                            "autoscaling:DescribeTags",
-                            "autoscaling:SetDesiredCapacity",
-                            "autoscaling:TerminateInstanceInAutoScalingGroup",
-                        ],
                         resources=["*"],
                     )
                 ]
@@ -93,10 +75,18 @@ instanceRole = Role(
     ),  # Protected as deletion will break assume role policies that reference this role
 )
 
-defaultRole = Role(
-    resource_name=f"{base_name}-default-pod-role",
+clusterAutoscalerRole = Role(
+    resource_name=f"{base_name}-cluster-autoscaler-role",
     assume_role_policy=get_policy_document(
         statements=[
+            GetPolicyDocumentStatementArgs(
+                principals=[
+                    GetPolicyDocumentStatementPrincipalArgs(
+                        identifiers=["ec2.amazonaws.com"], type="Service"
+                    )
+                ],
+                actions=["sts:AssumeRole"],
+            ),
             GetPolicyDocumentStatementArgs(
                 principals=[
                     GetPolicyDocumentStatementPrincipalArgs(
@@ -104,7 +94,53 @@ defaultRole = Role(
                     )
                 ],
                 actions=["sts:AssumeRole"],
-            )
+            ),
+        ]
+    ).json,
+    inline_policies=[
+        RoleInlinePolicyArgs(
+            name="cluster-autoscaler",
+            policy=get_policy_document(
+                statements=[
+                    GetPolicyDocumentStatementArgs(
+                        actions=[
+                            "autoscaling:DescribeAutoScalingGroups",
+                            "autoscaling:DescribeAutoScalingInstances",
+                            "autoscaling:DescribeLaunchConfigurations",
+                            "autoscaling:DescribeTags",
+                            "autoscaling:SetDesiredCapacity",
+                            "autoscaling:TerminateInstanceInAutoScalingGroup",
+                        ],
+                        resources=["*"],
+                    )
+                ]
+            ).json,
+        ),
+    ],
+    name=f"{base_name}-cluster-autoscaler-role",
+    tags=tagger.create_tags(f"{base_name}-cluster-autoscaler-role"),
+)
+
+defaultRole = Role(
+    resource_name=f"{base_name}-default-pod-role",
+    assume_role_policy=get_policy_document(
+        statements=[
+            GetPolicyDocumentStatementArgs(
+                principals=[
+                    GetPolicyDocumentStatementPrincipalArgs(
+                        identifiers=["ec2.amazonaws.com"], type="Service"
+                    )
+                ],
+                actions=["sts:AssumeRole"],
+            ),
+            GetPolicyDocumentStatementArgs(
+                principals=[
+                    GetPolicyDocumentStatementPrincipalArgs(
+                        identifiers=[instanceRole.arn], type="AWS"
+                    )
+                ],
+                actions=["sts:AssumeRole"],
+            ),
         ]
     ).json,
     name=f"{base_name}-default-pod-role",
