c45aeded4a91e069fb0ca61c6ddf042b09741a7f
remedon <bikes_28@hotmail.com>
2022-05-09 12:53:48 +0100

    uncommentd conflcting code

--- infra/vpc.py
+++ infra/vpc.py
@@ -146,14 +146,14 @@ for availability_zone, public_cidr_block, private_cidr_block in zip(
         subnet_id=privateSubnet.id,
         opts=ResourceOptions(parent=privateRouteTable),
     )
-    for name, cidr_block in vpc_config["transit_gateway"]["routes"]:
-        additionalPrivateRoute = Route(
-            resource_name=f"{base_name}-private-{availability_zone}-{name}",
-            destination_cidr_block=cidr_block,
-            transit_gateway_id=transitGateway.id,
-            route_table_id=privateRouteTable.id,
-            opts=ResourceOptions(depends_on=transitGateway, parent=privateRouteTable),
-        )
+    # for name, cidr_block in vpc_config["transit_gateway"]["routes"]:
+    #     additionalPrivateRoute = Route(
+    #         resource_name=f"{base_name}-private-{availability_zone}-{name}",
+    #         destination_cidr_block=cidr_block,
+    #         transit_gateway_id=transitGateway.id,
+    #         route_table_id=privateRouteTable.id,
+    #         opts=ResourceOptions(depends_on=transitGateway, parent=privateRouteTable),
+    #     )
 
 transitGatewayVpcAttachment = ec2transitgateway.VpcAttachment(
     resource_name=f"{base_name}",
