b703d9255cede5b65a7d34ed252755604d64b9e8
Calum Barnett <calum.barnett@digital.justice.gov.uk>
2022-05-30 14:18:44 +0100

    Add conditions to trust policy to improve security

--- infra/iam/roles.py
+++ infra/iam/roles.py
@@ -1,6 +1,7 @@
 from pulumi.resource import ResourceOptions
 from pulumi_aws.iam import (
     GetPolicyDocumentStatementArgs,
+    GetPolicyDocumentStatementConditionArgs,
     GetPolicyDocumentStatementPrincipalArgs,
     Role,
     RoleInlinePolicyArgs,
@@ -9,7 +10,7 @@ from pulumi_aws.iam import (
 )
 from pulumi_aws.iam.role_policy import RolePolicy
 
-from ..base import account_id, base_name, tagger
+from ..base import account_id, base_name, region, tagger
 
 executionRole = Role(
     resource_name=f"{base_name}-execution-role",
@@ -161,10 +162,22 @@ flowLogRole = Role(
             GetPolicyDocumentStatementArgs(
                 principals=[
                     GetPolicyDocumentStatementPrincipalArgs(
-                        identifiers=["vpc-flow-logs.amazonaws.com"], type="service"
+                        identifiers=["vpc-flow-logs.amazonaws.com"], type="Service"
                     )
                 ],
                 actions=["sts:AssumeRole"],
+                conditions=[
+                    GetPolicyDocumentStatementConditionArgs(
+                        test="StringEquals",
+                        values=[account_id],
+                        variable="aws:SourceAccount",
+                    ),
+                    GetPolicyDocumentStatementConditionArgs(
+                        test="ArnLike",
+                        values=[f"arn:aws:ec2:{region}:{account_id}:vpc-flow-log/*"],
+                        variable="aws:SourceArn",
+                    ),
+                ],
             )
         ]
     ).json,
