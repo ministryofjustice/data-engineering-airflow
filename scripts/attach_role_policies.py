37f54db1b9e815ec9c6a827c218b8940ce49e548
sbias <63045405+s-ducks@users.noreply.github.com>
2022-10-05 12:13:49 +0100

    add de teams to airflow ui access

--- scripts/attach_role_policies.py
+++ scripts/attach_role_policies.py
@@ -11,22 +11,34 @@ client = boto3.client("iam")
 paginator = client.get_paginator("list_roles")
 response_iterator = paginator.paginate()
 
-alpha_users = []
+de_team_roles = [
+    "soct-data-engineer",
+    "courts-data-engineer",
+    "data-first-data-science",
+    "prison-probation-data-engineer",
+    "opg-data-engineer",
+    "data-first-data-engineer",
+    "corporate-data-engineer",
+    "cc-data-engineer",
+]
+
+ui_access_users = []
 for item in response_iterator:
-    alpha_users.extend(
+    ui_access_users.extend(
         [
             role["RoleName"]
             for role in item["Roles"]
-            if role["RoleName"].startswith("alpha_user_")
+            if role["RoleName"].startswith("alpha_user_") or
+            role["RoleName"] in de_team_roles
         ]
     )
 
-for alpha_user in alpha_users:
+for iam_user in ui_access_users:
     client.attach_role_policy(
-        RoleName=alpha_user,
+        RoleName=iam_user,
         PolicyArn="arn:aws:iam::593291632749:policy/airflow-dev-ui-access",
     )
     client.attach_role_policy(
-        RoleName=alpha_user,
+        RoleName=iam_user,
         PolicyArn="arn:aws:iam::593291632749:policy/airflow-prod-ui-access",
     )
