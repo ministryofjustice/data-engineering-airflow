19d3ec4dab5de4d56b4e751d10911ae243a82741
LavMatt <mattlaverty@gmail.com>
2022-10-27 09:50:10 +0100

    adding account ids to providers

--- infra/providers.py
+++ infra/providers.py
@@ -11,6 +11,9 @@ infra_creator_role_name = os.getenv("INFRA_CREATOR_ROLE_NAME")
 data_arn = f"arn:aws:iam::593291632749:role/{infra_creator_role_name}"
 data_engineering_arn = f"arn:aws:iam::189157455002:role/{infra_creator_role_name}"
 
+data_account_id = "593291632749"
+data_engineering_id = "189157455002"
+
 dataProvider = Provider(
     resource_name="data",
     assume_role=ProviderAssumeRoleArgs(role_arn=data_arn),
