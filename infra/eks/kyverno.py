295d70a895d41569114c3a41b50c114ac848d059
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2022-11-17 20:30:32 +0000

    DATA-318 Kyverno Implementation (#93)
    * DATA-318 Initial Kyverno Implementation
    
    * DATA-318 Fix trailing space
    
    * DATA-318 Update kyverno with excluded namespaces
    
    * DATA-318 add kyverno to excluded namespaces as a precaution
    
    * DATA-318 re-add erroneously removed file
    
    * DATA-318 reduce duplication

---
+++ infra/eks/kyverno.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+
+import pulumi_kubernetes as k8s
+from pulumi import Alias, ResourceOptions
+
+from ..base import eks_config
+from .cluster import cluster, cluster_provider
+
+# See https://github.com/open-policy-agent/gatekeeper-library/
+
+kyverno_namespace = k8s.core.v1.Namespace(
+    resource_name="kyverno-system",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="kyverno",
+    ),
+    opts=ResourceOptions(
+        provider=cluster_provider, delete_before_replace=True, parent=cluster
+    ),
+)
+
+# Deploys Keyverno based on the chart specified in the stack .yaml
+kyverno = k8s.helm.v3.Release(
+    resource_name="kyverno",
+    chart="kyverno",
+    create_namespace=True,
+    repository_opts=k8s.helm.v3.RepositoryOptsArgs(
+        repo="https://kyverno.github.io/kyverno/"
+    ),
+    name="kyverno",
+    namespace=kyverno_namespace.metadata.name,
+    skip_await=False,
+    version=eks_config["kyverno"]["chart_version"],
+    opts=ResourceOptions(
+        provider=cluster_provider,
+        delete_before_replace=True,
+        parent=kyverno_namespace,
+        aliases=[Alias(parent=cluster)],
+    ),
+)
+
+# Generic path to append specific policy locations to
+policy_path = str(Path(__file__).parent) + "/policies/"
+
+excluded_namespaces = k8s.yaml.ConfigFile(
+    "kyverno-excluded-namespaces",
+    policy_path + "kyv.excluded_namespaces.yaml",
+    opts=ResourceOptions(
+        provider=cluster_provider, delete_before_replace=True, parent=kyverno
+    ),
+)
+
+kyverno_privilege = k8s.yaml.ConfigFile(
+    "kyverno-privilege-escalation",
+    policy_path + "kyv.privilege_escalation.yaml",
+    opts=ResourceOptions(
+        provider=cluster_provider,
+        delete_before_replace=True,
+        parent=excluded_namespaces,
+    ),
+)
+
+kyverno_non_root = k8s.yaml.ConfigFile(
+    "kyverno-non-root",
+    policy_path + "kyv.run_as_non_root.yaml",
+    opts=ResourceOptions(
+        provider=cluster_provider,
+        delete_before_replace=True,
+        parent=excluded_namespaces,
+    ),
+)
+
+kyverno_non_root_user = k8s.yaml.ConfigFile(
+    "kyverno-non-root-user",
+    policy_path + "kyv.run_as_non_root_user.yaml",
+    opts=ResourceOptions(
+        provider=cluster_provider,
+        delete_before_replace=True,
+        parent=excluded_namespaces,
+    ),
+)
